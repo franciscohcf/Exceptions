f9647371a6e0fd5386afdbfdc17f820fd84174ce
Francisco Holanda <franciscocavalcantefilho@gmail.com>
2021-05-11 15:45:52 -0300

    Finalizado

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,7 +1,39 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        method1();
+
+        System.out.println("End of program");
+
+    }
+
+    public static void method1() {
+        System.out.println("**Method1 START**");
+        method2();
+        System.out.println("**Method1 END**");
+    }
+
+    public static void method2() {
+        System.out.println("**Method2 START**");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split("  ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+            e.printStackTrace();
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+        sc.close();
+        System.out.println("**Method2 END**");
     }
 }
